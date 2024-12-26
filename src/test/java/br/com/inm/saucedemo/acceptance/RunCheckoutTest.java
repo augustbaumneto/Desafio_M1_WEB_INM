13f469cddf1d3b5083f587027e3bd40b46ee7785
August Baumgartner <augustbn@inmetrics.com.br>
2024-12-26 12:06:26 -0300

    configurado runners para ajustar nome do reporte

--- src/test/java/br/com/inm/saucedemo/acceptance/RunCheckoutTest.java
+++ src/test/java/br/com/inm/saucedemo/acceptance/RunCheckoutTest.java
@@ -3,6 +3,8 @@ package br.com.inm.saucedemo.acceptance;
 //import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.IncludeTags;
@@ -24,7 +26,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @SelectClasspathResource("features")			//Caminho das features
 @IncludeTags("checkout")							//Tags executadas
 @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true") //Configura para não exibir o quadro do cucumber no console
-//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty") //Configura para o Gherkin de cada step sair no console.
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:target/reports/cucumbercheckout.html,json:target/reports/cucumbercheckout.json") //Configura para o Gherkin de cada step sair no console.
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.com.inm.saucedemo.acceptance.steps") //Configura o caminho das classes com os steps
 public class RunCheckoutTest {
 }
